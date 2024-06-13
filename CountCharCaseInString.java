57fbddd283ee8d886e2a1d2751b4430b95ac4d48
Sayyid Nur Cahyo Abdul Jalil <sayyidnurc123@gmail.com>
2024-06-13 11:08:03 +0700

    finish function

--- CountCharCaseInString.java
+++ CountCharCaseInString.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class CountCharCaseInString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Masukkan string: ");
+        String input = scanner.nextLine();
+        CharCount(input);
+        scanner.close();
+    }
+
+    public static void CharCount(String input){
+        int upper = 0;
+        int lower = 0;
+        int digit = 0;
+        int symbol = 0;
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                upper++;
+            } else if (Character.isLowerCase(c)) {
+                lower++;
+            } else if (Character.isDigit(c)) {
+                digit++;
+            } else {
+                symbol++;
+            }
+        }
+        System.out.println("Uppercase : " + upper);
+        System.out.println("Lowercase : " + lower);
+        System.out.println("Angka : " + digit);
+        System.out.println("Symbol : " + symbol);
+    }
 }
